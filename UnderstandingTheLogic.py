f2d3130d0e2f443bbdee2fafe717cfa8b44ecf67
VictorMangs <victor.o.ouko@gmail.com>
2022-01-25 08:05:59 -0500

    Logic Practice

--- UnderstandingTheLogic.py
+++ UnderstandingTheLogic.py
@@ -31,9 +31,12 @@ def degree(nums):
 
 
 def sortByBits(arr):
+    #dictionary to hold binary lists based on how many 1s
     arr2 = {}
+    #Arr3 to replace arr1
     arr3 = []
     
+    #Loop through array and count bins for each element and add to dict
     for i in arr:
         numbin = bin(i).count(str('1'))
         if numbin not in arr2:
@@ -41,14 +44,41 @@ def sortByBits(arr):
         else:
             arr2[numbin].append(i)
     
+    #Sort the keys
     x = list(sorted(arr2.keys()))
     
+    #Loop through sorted keys and add sorted lists from dict to arr3
     for i in x:
         arr3+=sorted(arr2[i])
-        
+
+    #Return  
     arr = arr3
     return arr
 
+
+def backspaceCompare(s,t):
+    a = []
+    b = []
+    
+    for i in s:
+        if i=='#':
+            try:
+                a.pop()
+            except:
+                pass
+        else:
+            a.append(i)
+        
+    for i in t:
+        if i=='#':
+            try:
+                b.pop()
+            except:
+                pass
+        else:
+            b.append(i)
+
+    return a==b
 
 
 nums = [1,2,2,3,1]
